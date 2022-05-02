690b0e2ed50c8c435ab5a8e63221e957535ade0a
AaronCS25 <102536323+AaronCS25@users.noreply.github.com>
2022-05-02 13:57:10 -0500

    libro - autor

--- app.py
+++ app.py
@@ -1,124 +1,39 @@
-#imports
-#from crypt import methods
-from email.policy import default
-from flask import (
-    Flask,
-    jsonify, 
-    redirect, 
-    render_template, 
-    request, 
-    url_for,
-    abort
-)
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
-import psycopg2
 
-#configuration
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Magdalena150@localhost:5432/proyectodb'
+from sqlalchemy import false, true
+
+# Configuration
+app = Flask('__name__')
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost:5432/todoapp20'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app) # Instancia de SQLAlchemy
+migrate = Migrate(app, db) # Instancia de la aplicación + Instacia SQLAlchemy
 
-connection = psycopg2.connect('dbname=proyectodb user=postgres host=localhost password=Magdalena150 port=5432')
-cursor = connection.cursor()
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-#models
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-    usuario = db.Column(db.String, primary_key=True)
-    nombre = db.Column(db.String(), nullable=False)
-    apellidos = db.Column(db.String(), nullable=False)
-    sexo = db.Column(db.String(), nullable=False)
-    fecha = db.Column(db.Date(), nullable=False)
-    pais = db.Column(db.String(), nullable=False)
-    password = db.Column(db.String(), nullable=False)
-    
-    
-
-
+# Models
+class Libro(db.Model):
+    __tablename_ = 'libros'
+    nombre = db.Column(db.string(80), primary_key=True)
+    fecha_publicacion = db.Column(db.string(80), nullable=False)
+    editorial = db.Column(db.String(80), nullable=False)
+    nro_paginas = db.Column(db.Integer, nullable = False)
 
     def __repr__(self):
-        return f'Usuario: usuario={self.usuario}, nombre={self.nombre}'
+        return f'''nombre: {self.nombre},
+         fecha: {self.fecha_publicacion},
+          editorial: {self.editorial},
+          nro_paginas: {self.nro_paginas}'''
 
-
-db.create_all()
-SQL_INSERT = 'insert into usuarios(usuario, nombre, apellidos, sexo, fecha, pais, password) values(%(usuario)s, %(nombre)s, %(apellidos)s, %(sexo)s, %(fecha)s, %(pais)s, %(password)s)'
-
-
-connection.commit()
-cursor.close()
-connection.close()
-
-#controller
-@app.route('/')
-def index():
-    return render_template('index.html', usuarios=Usuario.query.order_by('nombre').all())
-
-@app.route('/usuarios/create', methods=['POST'])
-def create_todo_post():
-    error = False
-    response = {}
-    try:
-        usuario = request.get_json()['usuario']
-        nombre = request.get_json()['nombre']
-        apellidos = request.get_json()['apellidos']
-        sexo = request.get_json()['sexo']
-        fecha = request.get_json()['fecha']
-        pais = request.get_json()['pais']
-        password = request.get_json()['password']
-
-        todo = Todo(usuario=usuario, nombre=nombre, apellidos=apellidos, sexo=sexo, fecha=fecha, pais=pais, password=password)
-        db.session.add(todo)
-        db.session.commit()
-        response['usuario'] = usuario
-        response['nombre'] = nombre
-        response['apellidos'] = apellidos
-        response['sexo'] = sexo
-        response['fecha'] = fecha
-        response['pais'] = pais
-        response['password'] = password
-    except:
-        error = True
-        db.session.rollback()
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-
-    if error:
-        abort(500)
-    else:
-        return jsonify(response)
-
-
-@app.route('/usuarios/createnom', methods=['POST'])
-def create_todo_post_usuario():
-    error = False
-    response = {}
-    try:
-        usuario = request.get_json()['usuario']
-        todo = Todo(usuario = usuario)
-        db.session.add(todo)
-        db.session.commit()
-        response['usuario'] = usuario
-    except:
-        error = True
-        db.session.rollback()
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-
-    if error:
-        abort(500)
-    else:
-        return jsonify(response)
-
-
-
-#run
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+class Autor(db.Model):
+    __tablename__ = 'autores'
+    nombre = db.Column(db.String(80), primary_key=True)
+    apellido = db.Column(db.String(80), primary_key=True)
+    apodo = db.Column(db.String(80), nullable=True)
+    
+    def __repr__(self):
+        return f'''nombre: {self.nombre},
+         apellido: {self.apellido},
+         apodo: {self.apodo}'''
