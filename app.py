a9f264f54960e87ad749f1d302871a7ee2d028a2
AaronCS25 <102536323+AaronCS25@users.noreply.github.com>
2022-05-06 11:22:07 -0500

    libro - autor - usuario

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, redirect, render_template, request, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import sys
-from sqlalchemy import false, true
 
-# Configurations
+# Configuration
 app = Flask('__name__')
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost:5432/proyectodb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,33 +11,30 @@ db = SQLAlchemy(app) # Instancia de SQLAlchemy
 migrate = Migrate(app, db) # Instancia de la aplicación + Instacia SQLAlchemy
 
 # Models
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    nombre = db.Column(db.String(), nullable=False)
-    apellidos = db.Column(db.String(), nullable=False)
-    sexo = db.Column(db.String(), nullable=False)
-    edad = db.Column(db.Date, nullable=False)
-    pais = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(), primary_key=True)
     usuario = db.Column(db.String(), primary_key=True)
-    password = db.Column(db.String(), nullable=False)
+    contraseña = db.Column(db.String(), nullable=False)
+    nombre = db.Column(db.String(), nullable=False)
+    apellido = db.Column(db.String(), nullable=False)
+    edad = db.Column(db.Date, nullable=False)
     admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return f'''nombre: {self.nombre},
-            apellidos: {self.apellidos},
-            sexo: {self.sexo},
-            edad: {self.edad},
-            pais: {self.pais},
-            email: {self.email},
-            usuario: {self.usuario},
-            password: {self.password},
-            admin: {self.admin}'''
-    
+        return f'''email: {self.email},
+         usuario: {self.usuario},
+          contraseña: {self.contraseña},
+          nombre: {self.nombre},
+          apellido: {self.apellido},
+          edad: {self.edad},
+          admin: {self.admin}'''
+          
 class Libro(db.Model):
     __tablename_ = 'libros'
-    nombre = db.Column(db.string(80), primary_key=True)
-    fecha_publicacion = db.Column(db.string(80), nullable=False)
+    nombre = db.Column(db.String(80), primary_key=True)
+    fecha_publicacion = db.Column(db.String(80), nullable=False)
     editorial = db.Column(db.String(80), nullable=False)
     nro_paginas = db.Column(db.Integer, nullable = False)
 
@@ -59,3 +54,43 @@ class Autor(db.Model):
         return f'''nombre: {self.nombre},
          apellido: {self.apellido},
          apodo: {self.apodo}'''
+
+db.create_all()
+
+# Controllers 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/usuario/registrar', methods=['POST'])
+def registrar_usuario():
+    respuesta = {}
+    try:
+        nombre = request.get_json()['nombre']
+        apellido = request.get_json()['apellido']
+        edad = request.get_json()['edad']
+        email = request.get_json()['email']
+        usuario = request.get_json()['usuario']
+        contraseña = request.get_json()['contraseña']
+        user = Usuario(email=email,usuario=usuario,contraseña=contraseña,nombre=nombre,apellido=apellido,edad=edad)
+        print(user)
+        db.session.add(user)
+        db.session.commit()
+        respuesta['nombre'] = nombre
+        respuesta['apellido'] = apellido
+        respuesta['edad'] = edad
+        respuesta['email'] = email
+        respuesta['usuario'] = usuario
+        respuesta['contraseña'] = contraseña
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify(respuesta)
+
+
+
+# Run
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
